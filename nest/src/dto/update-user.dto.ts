100ca2a2ff8b13cb17e4b1ac9238c25021b5dc7c
nathandumont <nathan.dumont@webinage.fr>
2024-05-25 16:25:57 +0200

    delete & update

---
+++ nest/src/dto/update-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsNumber()
+  id: number;
+}
--- nest/src/repositories/adapters/user.adapter.ts
+++ nest/src/repositories/adapters/user.adapter.ts
@@ -3,8 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from 'src/application/ports/user.port';
 import { User } from 'src/domain/entities/user';
 import { UserSchema } from '../schemas/user.schema';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UserDto } from 'src/dto/user.dto';
+import { UpdateUserDto } from 'src/dto/update-user.dto';
 
 @Injectable()
 export class UserAdapter implements UserService {
@@ -17,9 +19,23 @@ export class UserAdapter implements UserService {
     return this.usersRepository.find();
   }
 
+  getUserById(userId: number): Promise<User | null> {
+    const id = userId.toString();
+    return this.usersRepository.findOneBy({ id });
+  }
+
   createUser(createUserDto: CreateUserDto): Promise<User> {
     const user = this.usersRepository.create(createUserDto);
     return this.usersRepository.save(user);
+  }
+
+  updateUser(
+    updateUserId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UpdateResult> {
+    const id = updateUserId.toString();
+    const userToUpdate = { ...updateUserDto, id: updateUserDto.id.toString() };
+    return this.usersRepository.update(id, userToUpdate);
   }
 
   findUser(user: CreateUserDto): Promise<User> {
@@ -29,5 +45,9 @@ export class UserAdapter implements UserService {
         firstName: user.firstName,
       },
     });
+  }
+
+  deleteUser(deleteUserId: number): Promise<DeleteResult> {
+    return this.usersRepository.delete(deleteUserId);
   }
 }
